697cbeb665cd364c95718aaa915703958501ef51
DanisBikmaev <bikmaevdv@gmail.com>
2023-07-12 12:52:52 +0500

    fix dropdown inputs and add jquery-ui.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,6 @@ import "~/app/libs/jquery.min.js";
 import "~/app/libs/swiper.min.js";
 import "~/app/libs/odometr.min.js";
 import "~/app/libs/jquery-ui.min.js";
+import "~/app/libs/jquery-ui.js";
 import "~/app/libs/custom.js";
 import "~/app/libs/newsdetail-services-script.js";
