99ba48736ab0905944f6c7791b6bc30c2ad50be5
DanisBikmaev <bikmaevdv@gmail.com>
2023-07-12 13:09:20 +0500

    add bootstrap 3.4.1

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,7 @@ import "~/app/libs/swiper.min.js";
 import "~/app/libs/odometr.min.js";
 import "~/app/libs/jquery-ui.min.js";
 import "~/app/libs/jquery-ui.js";
+import "~/app/libs/bootstrap.min.js";
+
 import "~/app/libs/custom.js";
 import "~/app/libs/newsdetail-services-script.js";
