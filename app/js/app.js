f6a7f80679576093aad36fbd7464cf9061ef5f8a
Kerish <kerish.turista@gmail.com>
2023-07-12 13:13:43 +0500

    Add general.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,9 @@
 import "~/app/libs/jquery.min.js";
+import "~/app/libs/jquery-migrate.js";
 import "~/app/libs/swiper.min.js";
 import "~/app/libs/odometr.min.js";
+import "~/app/libs/jquery-ui.js";
 import "~/app/libs/jquery-ui.min.js";
+import "~/app/libs/general.js";
 import "~/app/libs/custom.js";
 import "~/app/libs/newsdetail-services-script.js";
