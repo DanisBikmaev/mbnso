fdea2d170687873380c2b5f7b60c4d4a9a4d6dc6
Kerish <kerish.turista@gmail.com>
2023-07-13 11:21:34 +0500

    Fix general.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,18 @@
 import "~/app/libs/jquery.min.js";
 import "~/app/libs/jquery-migrate.js";
 import "~/app/libs/jquery.validate.min.js";
+import "~/app/libs/jquery.actual.min.js";
+import "~/app/libs/jquery.cookie.js";
+import "~/app/libs/jquery.fancybox.js";
+import "~/app/libs/jquery.alphanumeric.min.js";
+import "~/app/libs/jquery.appear.js";
 import "~/app/libs/swiper.min.js";
 import "~/app/libs/odometr.min.js";
 import "~/app/libs/jquery-ui.js";
 import "~/app/libs/jquery-ui.min.js";
-//import "~/app/libs/general.js";
+import "~/app/libs/general.js";
 import "~/app/libs/custom.js";
 import "~/app/libs/newsdetail-services-script.js";
 import "~/app/libs/bootstrap.js";
+
+//.swiperight
